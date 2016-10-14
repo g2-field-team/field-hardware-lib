4fa9cfe7d2d016161e621296f22be1508752efee
Matthias W. Smith <mwsmith2@uw.edu>
2016-10-13 22:13:54 -0700

    Adds a trigger class to the sis3316 example.

--- vme-hardware/examples/src/sis3316_test.cxx
+++ vme-hardware/examples/src/sis3316_test.cxx
@@ -1,23 +1,40 @@
 #include <fstream>
 #include "g2field/sis3316.hh"
+#include "g2field/dio_trigger_board.hh"
 
 
 int main(int argc, char *argv[])
 {
+  // WFD variables.
   int trace_len = 100000;
   int num_ch = 16;
+  int num_read_attempts = 10;
+  std::string wfd_name("Test WFD");
+  std::string wfd_conf("/usr/local/etc/g2field/sis3316_0.json");
+
+  // Trigger variables
+  auto trg_card(hw::BOARD_B);
+  int trg_port = 3;
+  int trg_mask = 15;
+
+  // Output
+  std::string outfile("output/sis3316_test_data.csv");
 
   // Open an output file.
   std::ofstream out;
-  out.open("output/sis3316_test_data.csv");
+  out.open(outfile);
 
   // Create the digitizer.
-  hw::Sis3316 wfd("Test WFD", "/usr/local/etc/g2field/sis3316_0.json", trace_len);
+  hw::Sis3316 wfd(wfd_name, wfd_conf, trace_len);
   hw::wfd_data_t data;
 
-  // Grab an event (well, try a few times anyway).
-  for (int i = 0; i < 10; ++i) {
+  // Create the trigger board.
+  hw::DioTriggerBoard trg(0x1000, trg_card, trg_port, false);
 
+  // Grab an event (well, try a few times anyway).
+  for (int i = 0; i < num_read_attempts; ++i) {
+
+    // Check if there is already a triggered event.
     if (wfd.DataAvailable()) {
       data = wfd.PopEvent();
       break;
@@ -25,8 +42,12 @@ int main(int argc, char *argv[])
     } else {
       usleep(200000);
     }
+
+    // Try to trigger the digitizer and NMR pulser.
+    trg.FireTriggers(trg_mask);
   }
 
+  // Save the waveforms.
   for (int idx = 0; idx < trace_len; ++idx) {
     for (int ch = 0; ch < num_ch; ++ch) {
       out << data.trace[ch][idx] << " ";
