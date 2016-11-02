09c9c2ac4f695f4025458161e44c86d2f9bc14b1
hongran <hongran@uw.edu>
2016-11-01 19:19:24 -0500

    update trolley interface

--- TrolleyInterface/src/TrolleyInterface.cxx
+++ TrolleyInterface/src/TrolleyInterface.cxx
@@ -69,7 +69,7 @@ namespace TrolleyInterface{
   // Connection Functions
   //==============================================================================
 
-  int DeviceConnect (char* deviceIP)
+  int DeviceConnect (const char * deviceIP)
   {
     int error = 0;
     ReadFromFile = false;
